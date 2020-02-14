d53b824e45c94cdfddd8e8810a25098bcd83b96e
Adianto Wibisono <awibisono@ripe.net>
2020-02-14 15:52:44 +0100

    Reactive client test

---
+++ rpki-rtr-server/src/test/java/net/ripe/rpki/rtr/ReactorClient.java
@@ -0,0 +1,37 @@
+package net.ripe.rpki.rtr;
+
+import net.ripe.rpki.rtr.adapter.netty.PduCodec;
+import net.ripe.rpki.rtr.domain.SerialNumber;
+import net.ripe.rpki.rtr.domain.pdus.Pdu;
+import net.ripe.rpki.rtr.domain.pdus.ResetQueryPdu;
+import net.ripe.rpki.rtr.domain.pdus.SerialQueryPdu;
+import reactor.core.publisher.Mono;
+import reactor.netty.Connection;
+import reactor.netty.tcp.TcpClient;
+
+import static net.ripe.rpki.rtr.domain.pdus.ProtocolVersion.V0;
+
+public class ReactorClient {
+
+    public static void main(String[] args) {
+
+        final short sessionId = (short) ( 0);
+        final SerialNumber serial = SerialNumber.of(0);
+
+        Mono<Pdu> serialQueryPdu = Mono.just(SerialQueryPdu.of(V0, sessionId, serial));
+
+        Mono<Pdu> resetQueryPdu = Mono.just(ResetQueryPdu.of(V0));
+        Connection connection =
+                TcpClient.create()
+                        .wiretap(true)
+                        .host("localhost")
+                        .port(8082)
+                        .doOnConnected(conn->conn.addHandler(new PduCodec()))
+                        .handle((inboud, outbound) -> outbound.sendObject(resetQueryPdu))
+                        .connectNow();
+
+        connection.inbound().receiveObject().then().doOnNext(s-> System.out.println("Receiving = " + s)).subscribe();
+        connection.bind();
+        connection.onDispose().block();
+    }
+}
